9815348c534a1e8f738a95da96c279222a8d7237
ScarletAraujo <scarlet.araujo@gmail.com>
2021-03-18 13:15:57 -0300

    Atividade Spring

---
+++ bolggeneration17/bolggeneration17/src/main/java/org/scarletaraujo/bolggeneration17/Bolggeneration17Application.java
@@ -0,0 +1,14 @@
+package org.scarletaraujo.bolggeneration17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bolggeneration17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bolggeneration17Application.class, args);
+	}
+
+	
+}
---
+++ bolggeneration17/bolggeneration17/src/test/java/org/scarletaraujo/bolggeneration17/Bolggeneration17ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.scarletaraujo.bolggeneration17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bolggeneration17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
